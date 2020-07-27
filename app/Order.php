a9bfdffdf9ced89930687b589a175297cf3688c5
Agustin Amato <agustin.amato96@gmail.com>
2020-07-27 00:22:19 -0300

    [models] agrego relaciones

--- app/Order.php
+++ app/Order.php
@@ -9,8 +9,19 @@ class Order extends Model
 {
     use SoftDeletes;
 
+    /**
+     * Get the details for the order.
+     */
     public function details()
     {
         return $this->hasMany('App\OrderDetail');
+    }
+
+    /**
+     * Get the user record associated with the order.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
     }
 }
