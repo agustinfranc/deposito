150cb8d78721d4ef22a84dd7f49fda6c5c4ec020
Agustin Amato <agustin.amato96@gmail.com>
2020-08-03 20:00:16 -0300

    [stock] ajuste menor

--- resources/views/stock/edit.blade.php
+++ resources/views/stock/edit.blade.php
@@ -34,7 +34,7 @@
                         </div>
                         <div class="form-group">
                             <label for="price">Precio</label>
-                            <input type="number" class="form-control" id="price" name="price" value="{{ $stock->price }}" required>
+                            <input type="number" step=".01" class="form-control" id="price" name="price" value="{{ $stock->price }}" required>
                         </div>
                         <div class="form-group">
                             <label for="quantity">Cantidad</label>
